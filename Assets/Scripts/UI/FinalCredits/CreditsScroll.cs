d1a27d2578f519109ec88e507a62e123bc946cb6
DinisAzevedo <pedrodinisazevedo08@gmail.com>
2026-06-02 22:41:07 +0100

    FinalCreditsSystem

---
+++ Assets/Scripts/UI/FinalCredits/CreditsScroll.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScroll : MonoBehaviour
+{
+    public float speed = 50f;
+
+    [SerializeField]
+    private int menuSceneBuildIndex = 0;
+
+    [SerializeField]
+    private RectTransform endPoint;
+
+    private RectTransform rectTransform;
+    private bool finished;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (finished) return;
+
+        rectTransform.anchoredPosition += Vector2.up * speed * Time.deltaTime;
+
+        if (rectTransform.position.y >= endPoint.position.y)
+        {
+            finished = true;
+            SceneManager.LoadScene(menuSceneBuildIndex);
+        }
+    }
+}
