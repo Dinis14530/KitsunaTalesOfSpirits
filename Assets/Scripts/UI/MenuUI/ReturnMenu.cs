e1b477f10894acc1dea3a5cf8cda104314470845
DinisAzevedo <pedrodinisazevedo08@gmail.com>
2026-06-09 08:47:18 +0100

    Testing Deploy

---
+++ Assets/Scripts/UI/MenuUI/ReturnMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnMenu : MonoBehaviour
+{
+    [System.Obsolete]
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Salva o jogo antes de voltar ao menu
+            var playerSave = FindObjectOfType<PlayerSave>();
+            if (playerSave != null)
+                playerSave.SaveGame();
+
+            SceneManager.LoadScene(0);
+            Debug.Log(Application.persistentDataPath);
+        }
+    }
+}
